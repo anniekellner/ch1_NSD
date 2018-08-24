4fba1959a5b22001b8955f034178c8b3577b9919
Annie <annie.kellner@colostate.edu>
2018-08-24 14:55:23 -0600

    one

--- refda_NAs.R
+++ refda_NAs.R
@@ -15,7 +15,10 @@ colnames(sum) <- c("burst","dt_seconds")
 sum <- mutate(sum, hours=dt_seconds/3600) #convert seconds to hours
 
 #write.csv(sum, file="fix_intervals.csv")
-
-
+pb.traj.df$burst <- as.character(pb.traj.df$burst)
+one <- subset(pb.traj.df, burst== "pb_06810_2008" | burst=="pb_20414_2009" | burst=="pb_20446_2009" | burst=="pb_20520_2012"|
+              burst=='pb_20586_2008' | burst=='pb_20735_2009' | burst=='pb_20741_2008' | burst=='pb_20845_2015' | burst=='pb_20965_2008'|
+              burst=='pb_20966_2008'| burst=='pb_20975_2008'| burst=='pb_20982_2008'| burst=='pb_21343_2015'| burst=='pb_21380_2015'|
+              burst=='pb_32255_2008'| burst=='pb_32282_2008' | burst=='pb_32608_2008'| burst== 'pb_32698_2015')
 
 
